f016a80eae66fabb06109c8ba138e09397b22a3f
Dvwnmark <xcmark141@gmail.com>
2024-08-13 16:57:12 +0700

    add readme

--- csharp-tutorial/csharp-tutorial/obj/Release/net8.0/csharp-tutorial.AssemblyInfo.cs
+++ csharp-tutorial/csharp-tutorial/obj/Release/net8.0/csharp-tutorial.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-tutorial")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a768819db563d8a39a9af4d38f3da43c25062fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-tutorial")]
