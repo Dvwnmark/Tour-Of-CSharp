c368f5e59e4060f75872df70ca8ff2ce5ab2a31c
Dvwnmark <xcmark141@gmail.com>
2024-08-13 16:30:28 +0700

    hello world

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");*/
+
+using System;
+
+namespace _1_HelloWorld 
+{ 
+public class Program()
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.ReadLine();
+        }
+    }
+}
+
